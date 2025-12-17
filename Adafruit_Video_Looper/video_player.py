dfe98c5af5a1b6f0dd0e6c553764f4824f79ad88
zjyxwen <zhjnyi.wen@gmail.com>
2025-12-17 00:38:20 -0800

    Update video_player.py

--- Adafruit_Video_Looper/video_player.py
+++ Adafruit_Video_Looper/video_player.py
@@ -12,34 +12,29 @@ class VideoPlayer:
 
     def play(self, movie, loop=None):
         self.stop()
+        args = [
+            'ffplay',
+            '-fs',
+            '-loglevel', 'quiet',
+        ]
+        if loop == -1:
+            # -loop 0 means infinite seamless looping within ffplay itself
+            args.extend(['-loop', '0'])
+        else:
+            # Single play then exit
+            args.append('-autoexit')
+        args.append(movie.filename)
         env = os.environ.copy()
         env['DISPLAY'] = ':0'
-        if loop == -1:
-            script = 'while true; do ffplay -fs -autoexit -loglevel quiet "{}"; done'.format(movie.filename)
-            self._process = subprocess.Popen(
-                ['bash', '-c', script],
-                stdin=subprocess.DEVNULL,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                env=env,
-                preexec_fn=os.setsid
-            )
-        else:
-            args = [
-                'ffplay',
-                '-fs',
-                '-autoexit',
-                '-loglevel', 'quiet',
-                movie.filename
-            ]
-            self._process = subprocess.Popen(
-                args,
-                stdin=subprocess.DEVNULL,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                env=env,
-                preexec_fn=lambda: signal.signal(signal.SIGINT, signal.SIG_IGN)
-            )
+        env['SDL_AUDIODRIVER'] = 'alsa'
+        self._process = subprocess.Popen(
+            args,
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            env=env,
+            preexec_fn=lambda: signal.signal(signal.SIGINT, signal.SIG_IGN)
+        )
 
     def is_playing(self):
         if self._process is None:
@@ -48,15 +43,9 @@ class VideoPlayer:
 
     def stop(self):
         if self._process is not None:
-            try:
-                os.killpg(os.getpgid(self._process.pid), signal.SIGTERM)
-            except (ProcessLookupError, OSError):
-                pass
+            self._process.terminate()
             try:
                 self._process.wait(timeout=5)
             except subprocess.TimeoutExpired:
-                try:
-                    os.killpg(os.getpgid(self._process.pid), signal.SIGKILL)
-                except (ProcessLookupError, OSError):
-                    pass
+                self._process.kill()
             self._process = None
